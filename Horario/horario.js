5425da6a7813c1a1fb7eee3af7185c74f9df3039
Cristiane Gitti <95327853+crisgitti@users.noreply.github.com>
2022-02-01 23:43:11 -0300

    +++

--- Horario/horario.js
+++
@@ -1,7 +0,0 @@
-function (carregar) {
-    var msg = document.getElementById("msg")
-    var imagem = document.getElementById("img")
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora}`
-}
